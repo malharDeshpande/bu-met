9b14ace8ff772fb26b675d27a8a16de326e57080
Terry Lorber <tgl@rideside.net>
2010-02-22 21:30:12 +0000

    ENH final result

--- cs272/HW4/b2d-tgl.asm
+++ cs272/HW4/b2d-tgl.asm
@@ -1,9 +1,30 @@
-;BinDec.Asm
+;b2d-tgl.asm
+
+; Terry Lorber U61244526  21-Feb-2010
+; Spring 2010 CS272     
+
+; HW4
+
+; http://www.csgnetwork.com/asciiset.html
+; a null character is ^@
+; should be enterable as Ctrl-Shift-2 or Ctrl-2
+
+; The following values can be achieved by entering the
+; keystrokes on the right:
+        
+; 1D    ^@ ^A  
+; 2D    ^@ ^B
+; 3D    ^@ ^C
+; 128D  -- don't believe this is possible, there is no character associated with 128D in LATIN-1
+; 256D  ^A ^@
+; 1024D ^D ^@
 
 Data Segment
  A_Word_M   DB "Type any 2 characters (16-bits), "
-            DB "program will display in Decimal "
+            DB "program will display in Decimal:"
             DB 0AH, 0DH, "$"
+
+ Carriage_Return DB 0AH, 0DH, "$"        
 Data EndS
 
 Working_Storage Segment
@@ -29,6 +50,10 @@ Prog_S: Mov  AX, DATA
         Int  21H
         Mov  BL, AL      ;second byte to BL
 
+        Mov  DX, OFFSET Carriage_Return
+        MOV  AH, 9H
+        INT  21H
+        
         Mov  AX, BX      ;Move the 16-bits to AX for Conversion
         Mov  CX, 5       ;Loop counter
 
