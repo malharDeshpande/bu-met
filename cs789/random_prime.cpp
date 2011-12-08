18ab1251cbb55c5145881b39d29b3d2463e2036f
Terry Lorber <tgl@rideside.net>
2011-12-08 05:39:32 +0000

    ENH report duration

--- cs789/random_prime.cpp
+++ cs789/random_prime.cpp
@@ -14,17 +14,23 @@ main(int argc, char* argv[])
     n = ::atoi(argv[3]);
     k = ::atoi(argv[4]);
 
+    time_t start, end;
+    ::time(&start);
+
     tgl::BlumBlumShub bbs;
     tgl::BigInteger r = bbs.generate(p, q, n);
     tgl::MillerRabinTest test;
-
+    
     bool witness;
     while (!(witness = test.witness(r, k))) {
       r = bbs.generate(p, q, n);
     }
 
+    ::time(&end);
+
     std::cout << "RAND: " << r << std::endl;
     std::cout << "Miller-Rabin test is " << witness << std::endl;
+    std::cout << "Seconds to compute: " << ::difftime(end, start) << std::endl;
 
   } else {
     std::cerr << "Usage: " << argv[0] << " P Q N K" << std::endl;
