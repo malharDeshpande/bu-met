a9af1d62d228a1cebc46d509d1ad2e879ac92793
Terry Lorber <tgl@rideside.net>
2012-05-16 02:06:23 +0000

    ENH remove std::cout

--- cs799/lib/ElGamalVerify.cpp
+++ cs799/lib/ElGamalVerify.cpp
@@ -20,25 +20,12 @@ ElGamalVerify::run(const tgl::BigInteger& e1,
 {
   tgl::ExponentiationAlgorithm ea;
 
-  time_t start, end;
-  ::time(&start);
-
   _v1 = ea.pow_mod(e1, m, p);
 
   tgl::BigInteger a = ea.pow_mod(e2, s1, p);
   tgl::BigInteger b = ea.pow_mod(s1, s2, p);
 
   _v2 = (a * b ) % p;
-
-  std::cout << "Verify (V1, V2)\n"
-            << "V1 -> " << _v1 << "\n"
-            << "V2 -> " << _v2 << std::endl;
-  std::cout << "[" << ((_v1 == _v2) ? "TRUE" : "FALSE") << "]" << std::endl;
-    
-  ::time(&end);
-
-  std::cout << "Verifing took " << ::difftime(end, start) << "secs."
-            << std::endl;
 
   return;
 }// run
