9b1ebda67d43810cb189ba05bf08fbab0ae64fdb
Terry Lorber <tgl@rideside.net>
2012-05-16 02:08:18 +0000

    ENH remove std::cout

--- cs799/lib/ElGamalSign.cpp
+++ cs799/lib/ElGamalSign.cpp
@@ -29,14 +29,13 @@ ElGamalSign::run(const BigInteger& e1,
 
   tgl::ExponentiationAlgorithm ea;
 
-  std::cout << "TRACE - e1 r p " << e1 << " " << _r << " " << p << std::endl;
   _s1 = ea.pow_mod(e1, _r, p);
-  std::cout << "TRACE - S1 " << _s1 << std::endl;
+
   tgl::BigInteger x, y;
 
   tgl::BigInteger zero(0);
   tgl::BigInteger one(1);
-  tgl::BigInteger p_minus_1 = p - one;
+  tgl::BigInteger p_minus_1(p - one);
 
   tgl::ExpandedEuclideanAlgorithm eea;
   eea.extendedGcd(_r, p_minus_1, x, y);
@@ -44,24 +43,8 @@ ElGamalSign::run(const BigInteger& e1,
   if (x < zero) {
     x = p_minus_1 + x;
   }
-
   
-  _s2 = ((m - (d * _s1)) * x)  % (p_minus_1);
-
-  std::cout << "Random secret (r)\n"
-            << "r -> " << _r << std::endl;
-
-  std::cout << "Signatures (S1, S2)\n"
-            << "S1 -> " << _s1 << "\n"
-            << "S2 -> " << _s2 << std::endl;
-
-  time_t start, end;
-  ::time(&start);
-
-  ::time(&end);
-
-  std::cout << "Signing took " << ::difftime(end, start) << "secs."
-            << std::endl;
+  _s2 = ((m - (d * _s1)) * x) % p_minus_1;
 
   return;
 }// run
