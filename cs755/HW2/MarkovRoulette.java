3d91922cc8b8a0178f74e4e72ae242232a01df4c
terryg <tgl@rideside.net>
2013-07-01 15:46:19 -0400

    ENH latest Markov simulation, start of Species Lab

--- cs755/HW2/MarkovRoulette.java
+++ cs755/HW2/MarkovRoulette.java
@@ -55,7 +55,7 @@ public class MarkovRoulette
 	T.set(states-1, states-1, 1.); // no change, a winner is a winner
 	
 	// look see
-	System.out.println("This is the 'spin' matrix...");
+	System.out.println("This is the transition matrix...");
 	T.print(states, 2);
 
 	System.out.println("Each state represents this value 'in hand':");
@@ -83,44 +83,41 @@ public class MarkovRoulette
 
 	Matrix P = new Matrix(states, 1);
 	P.set(initial_state, 0, 1.);
+	Matrix P0 = new Matrix(P.getArray());
 	
-	System.out.println("Here are the probability vectors for each run...");
+
 	for (int idx = 0; idx < runs; idx++) {
-	    System.out.printf("P%d: ", idx);
-	    P.print(states, 3);
+            //	    System.out.printf("P%d: ", idx);
+            //	    P.print(states, 3);
 
 	    P = T.times(P);
 	}
 	
-	System.out.printf("P%d: ", runs);
+	System.out.println("The probability vector for the final run...");     
+        System.out.printf("P%d: ", runs);
 	P.print(states, 3);
+
+
+        // another way...
+	System.out.println("Now do it by raising T to the 'runs' power...");
+
+        Matrix R = new Matrix(T.getArray());
+	for (int idx = 0; idx < runs; idx++) {
+            R = R.times(R);
+	}
+
+        System.out.printf("R^%d: ", runs);
+        R.print(states, 3);
+
+        System.out.printf("P%d: ", runs);
+        R.times(P0).print(states, 3);
+        
+
 
 	// use eigenvalue decomposition
 	System.out.println("OK, now see what eigenvalue decomposition gives us...");
 
-
-        // Special transition matrix.  States 0 & (n-1) always
-        // transition to themselves.  Because of the betting
-        // strategey, from the starting state, one can either move up
-        // or down one state.
-        
-        Matrix S = new Matrix(states,states);
-	for (int idx = 0; idx < states; idx++) {
-	    if (idx > 0 && idx < (states - 1)) {
-		S.set(idx - 1, idx, 1.); // a loss
-		S.set(idx + 1, idx, 1.); // a win
-	    }
-	}
-        
-        S.set(0,0,1.);
-        S.set(states-1,states-1,1.);
-
-	// look see
-	System.out.println("This is the special transition matrix...");
-	S.print(states, 2);
-
-
-	EigenvalueDecomposition E = S.times(T).eig();
+	EigenvalueDecomposition E = T.eig();
 
 	System.out.println("Eigenvalues:");
 	E.getD().print(states, 3);
@@ -132,7 +129,6 @@ public class MarkovRoulette
 
         // find the dominant eigenvector, ignore imaginary
         // values when comparing.
-        double e = 0.0;
         double v[] = new double[states];
 	for (int idx = 0; idx <  E.getRealEigenvalues().length; idx++) {
 	    System.out.printf("%d : %f %f\n", 
@@ -146,36 +142,41 @@ public class MarkovRoulette
 
             double ei = E.getRealEigenvalues()[idx];
 
-            if (Math.abs(ei) > Math.abs(e)) {
-                e = ei;
+            if (Math.abs(ei - 1) < 1.0e-15) {
+                System.out.println("Found one!");
 
                 for (int row = 0; row < E.getV().getRowDimension(); row++) {
                     v[row] = E.getV().get(row, idx);
                 }
+
+                // while need to normalize
+
+                System.out.printf("The selected eigenvector for eigenvalue %.3f:\n", ei);
+
+                double sv = 0.0;
+                for (int indx = 0; indx < states; indx++) {
+                    System.out.printf("   %.3f", v[indx]);
+                    sv += v[indx];
+                }
+                
+                System.out.printf("\nScale by %.3f\n", sv);
+                
+                double[] p = new double[states];
+                for (int indx = 0; indx < states; indx++) {
+                    p[indx] = v[indx] / sv;
+                }
+                
+                System.out.println("Probabilities by eigenvalue decomposition...");
+                
+                for (int indx = 0; indx < states; indx++) {
+                    System.out.printf("   %.3f", p[indx]);
+                }
+
+                System.out.println("");
+
             }
 	}
 
-        // while need to normalize
-
-        System.out.printf("The selected eigenvector for eigenvalue %.3f:\n", e);
-
-        double sv = 0.0;
-        for (int idx = 0; idx < states; idx++) {
-            System.out.printf("   %.3f", v[idx]);
-            sv += v[idx];
-        }
-
-        System.out.printf("\nScale by %.3f\n", sv);
-
-        double[] p = new double[states];
-        for (int idx = 0; idx < states; idx++) {
-            p[idx] = v[idx] / sv;
-        }
-
-        System.out.println("Probabilities by eigenvalue decomposition...");
-        for (int indx = 0; indx < states; indx++) {
-            System.out.printf("   %.3f", p[indx]);
-        }
         
         System.out.println("\n\nFINI");
     }// Main
